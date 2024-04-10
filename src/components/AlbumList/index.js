16a70be877cf07db026f4513dd0bad5599b9d0d8
jaybera <jaybera119@gmail.com>
2024-04-10 08:54:05 +0530

    Add files via upload

--- src/components/AlbumList/index.js
+++ src/components/AlbumList/index.js
@@ -3,29 +3,31 @@ import React, { useEffect, useState } from 'react';
 const AlbumList = ({ userId }) => {
   const [albums, setAlbums] = useState([]);
 
-    const getAlbumList = async (userId) => {
-      try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`);
-        const data = await response.json();
-          
-        setAlbums(data);
-      } catch(error) {
-        console.log(error);
-      }
+  const getAlbumList = async (userId) => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/albums?userId=${userId}`
+      );
+      const data = await response.json();
+
+      setAlbums(data);
+    } catch (error) {
+      console.log(error);
     }
-  
+  };
 
   useEffect(() => {
-    getAlbumList(userId);
+    if (userId) getAlbumList(userId);
     // fetch(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`)
-		// 		.then((response) => response.json())
-		// 		.then((data) => setAlbums(data));
+    // 		.then((response) => response.json())
+    // 		.then((data) => setAlbums(data));
   }, [userId]);
 
+  console.log('albums ::', albums);
 
   return (
-    <ul className='albumList'>
-      {albums.map(album => (
+    <ul className="albumList">
+      {albums.map((album) => (
         <li key={album.id}>
           <a href={`/album/${album.id}`}>{album.title}</a>
         </li>
